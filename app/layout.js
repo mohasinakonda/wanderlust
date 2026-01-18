c2436dd5b5a374ee5f6ecb8515ebcfa7cecedd7d
mohasinakonda <mohasinakonda39@gmail.com>
2026-01-18 22:20:14 +0600

    project initialize

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable}  bg-white text-slate-600 antialiased selection:bg-emerald-100 selection:text-emerald-900`}
       >
         {children}
       </body>
