4480cdc52aad1bbfb5cd43c9bcbf129b6ae104a3
mohasinakonda <mohasinakonda39@gmail.com>
2026-01-22 23:22:45 +0600

    fix home page linking

--- components/common/plan-nav.jsx
+++ components/common/plan-nav.jsx
@@ -4,7 +4,7 @@ import { ShareButton } from "../share-btn"
 export const PlanNav = () => {
 
   return <nav className="absolute top-0 left-0 right-0 z-50 w-full py-6 px-6 lg:px-12 flex justify-between items-center text-white">
-    <a href="./index.html">
+    <a href="/">
       <div className="flex items-center gap-2">
         <Map className="w-6 h-6" />
         <span className="text-lg font-medium tracking-tight">Wanderlust</span>
